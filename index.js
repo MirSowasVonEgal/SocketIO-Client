9f5fe8ced9e0d994ed7795e80eeeb9e0645a4aa9
timoo <timootten@icloud.com>
2022-03-09 17:22:23 +0100

    Updated

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { io } = require("socket.io-client");
 const socket = io("ws://localhost:3000");
 socket.on("connect", () => {
-  socket.emit("auth:login", { username: "Test", password: "Passwort"}, (test) => {
+  socket.emit("auth:login", { username: "Test5", password: "ABC"}, (test) => {
     console.log(test)
   });
-})
+});
 
+socket.on("user:profile", (user) => {
+  console.log(user);
+});
